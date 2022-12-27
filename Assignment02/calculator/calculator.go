d4240dc3f6a70f46e7a3c20655e8cc8490fac220
shashidhar269 <shashidharreddy269@gmail.com>
2022-12-27 11:12:01 -0600

    assignment04

--- Assignment02/calculator/calculator.go
+++ Assignment02/calculator/calculator.go
@@ -1,19 +1,19 @@
 package calculator
 
-func Add(a, b int) int {
-	var addition = a + b
+func Add(a, b *int) int {
+	addition := *a + *b
 	return addition
 }
-func Sub(a, b int) int {
-	subtract := a - b
+func Sub(a, b *int) int {
+	subtract := *a - *b
 	return subtract
 }
-func Div(a, b int) (int, int) {
+func Div(a, b *int) (int, int) {
 
-	c, d := a/b, a%b
+	c, d := *a / *b, *a%*b
 	return c, d
 }
-func Mul(a, b int) int {
-	var multiplication = a * b
+func Mul(a, b *int) int {
+	var multiplication = *a * *b
 	return multiplication
 }
